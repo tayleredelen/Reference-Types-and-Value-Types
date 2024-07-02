83acc43245009479b285b711c0dea2f01157a378
tayleredelen <taylerledelen@gmail.com>
2024-07-02 12:46:34 -0500

    chapter complete

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace CSharpFundamentals
             //value of a at this point? 10 cuz int are value types
             //when value is copied to other variable, the copy is stored in target location in memory
             Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
+            //returns a: 10, b: 11
         }
 
     }
