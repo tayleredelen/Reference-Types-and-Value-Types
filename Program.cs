3a380e1cb3c40ed1ec9ec45552c5b12b97b0d0cb
tayleredelen <taylerledelen@gmail.com>
2024-07-02 12:54:21 -0500

    updated notes

--- Program.cs
+++ Program.cs
@@ -13,6 +13,12 @@ namespace CSharpFundamentals
             //when value is copied to other variable, the copy is stored in target location in memory
             Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
             //returns a: 10, b: 11
+
+            var array1 = new int[3]{1, 2, 3};
+            var array2 = array1;
+            array2[0] = 0;
+            //array1 1st element value at this point? I think it's 0
+            Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
         }
 
     }
